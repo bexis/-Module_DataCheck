1509f2378787a1d2a6b8c9816b7da4dee9f1dfd9
David Schöne <david.blaa@googlemail.com>
2019-12-17 15:07:59 +0100

    add projects

---
+++ BExIS.Modules.TEMPLATE.UI/Helper/TEMPLATESeedDataGenerator.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace BExIS.Modules.TEMPLATE.UI.Helpers
+{
+    public class TEMPLATESeedDataGenerator : IDisposable
+    {
+        public void GenerateSeedData()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ BExIS.TEMPLATE.Entities/Data/Example.cs
@@ -0,0 +1,16 @@
+using Vaiona.Entities.Common;
+
+namespace BExIS.TEMPLATE.Entities.Data
+{
+    public class Example : BaseEntity
+    {
+        #region Attributes
+
+        public virtual string Name { get; set; }
+
+
+        #endregion
+
+
+    }
+}
