fbd9eeed599ad32dad622ff9396bc0d6f29a8fc9
Petzold <eleonora.petzold@uni-jena.de>
2022-05-11 12:07:47 +0200

    first push

---
+++ BExIS.DPT_BE.Entities/Data/Example.cs
@@ -0,0 +1,16 @@
+using Vaiona.Entities.Common;
+
+namespace BExIS.DPT_BE.Entities.Data
+{
+    public class Example : BaseEntity
+    {
+        #region Attributes
+
+        public virtual string Name { get; set; }
+
+
+        #endregion
+
+
+    }
+}
---
+++ BExIS.Modules.DPT_BE.UI/Helper/DPT_BESeedDataGenerator.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace BExIS.Modules.DPT_BE.UI.Helpers
+{
+    public class DPT_BESeedDataGenerator : IDisposable
+    {
+        public void GenerateSeedData()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
